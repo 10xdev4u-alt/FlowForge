faecf21f625864e22f484f1e108844a66e8f6d04
10xdev4u-alt <10xdev4u@gmail.com>
2026-03-22 23:19:00 +0530

    feat: initialize database connection pool with pgxpool

---
+++ internal/database/database.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+	"context"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func InitDB(url string) (*pgxpool.Pool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	config, err := pgxpool.ParseConfig(url)
+	if err != nil {
+		return nil, err
+	}
+
+	pool, err := pgxpool.NewWithConfig(ctx, config)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := pool.Ping(ctx); err != nil {
+		return nil, err
+	}
+
+	return pool, nil
+}
