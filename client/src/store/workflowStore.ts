53a3b46981876d51e052eee1d9984b0247fc1977
10xdev4u-alt <10xdev4u@gmail.com>
2026-03-22 23:48:00 +0530

    feat: setup Zustand store for React Flow nodes and edges

---
+++ client/src/store/workflowStore.ts
@@ -0,0 +1,43 @@
+import { create } from 'zustand';
+import { 
+  Connection, 
+  Edge, 
+  EdgeChange, 
+  Node, 
+  NodeChange, 
+  addEdge, 
+  applyNodeChanges, 
+  applyEdgeChanges 
+} from 'reactflow';
+
+interface WorkflowState {
+  nodes: Node[];
+  edges: Edge[];
+  onNodesChange: (changes: NodeChange[]) => void;
+  onEdgesChange: (changes: EdgeChange[]) => void;
+  onConnect: (connection: Connection) => void;
+  setNodes: (nodes: Node[]) => void;
+  setEdges: (edges: Edge[]) => void;
+}
+
+export const useWorkflowStore = create<WorkflowState>((set, get) => ({
+  nodes: [],
+  edges: [],
+  onNodesChange: (changes) => {
+    set({
+      nodes: applyNodeChanges(changes, get().nodes),
+    });
+  },
+  onEdgesChange: (changes) => {
+    set({
+      edges: applyEdgeChanges(changes, get().edges),
+    });
+  },
+  onConnect: (connection) => {
+    set({
+      edges: addEdge(connection, get().edges),
+    });
+  },
+  setNodes: (nodes) => set({ nodes }),
+  setEdges: (edges) => set({ edges }),
+}));
